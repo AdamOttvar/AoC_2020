73b58b0519ee2eb2e85e05c22b7e9b1b3de8bf2d
Adam Ottvar <adam.ottvar@gmail.com>
2020-12-03 08:56:40 +0100

    Day3, cleaned up a bit

--- src/day03.cpp
+++ src/day03.cpp
@@ -3,44 +3,28 @@
 //
 
 #include "day03.h"
+#include <numeric>
 
 unsigned long day03(bool part_two) {
     cout << "AoC day03: part " << (part_two ? "two" : "one") << endl;
     auto slopeMap = AoC::read_file<string, vector<string>>("input/input03.txt");
-
-    vector<vector<int>> slopes;
-    if (!part_two) {
-        slopes = {{3, 1}};
-    } else {
-        slopes = {{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}};
-    }
+    vector<pair<int, int>> slopes{{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}};
 
     vector<int> nbrOfTreesInSlopes;
+    int slopeWidth = slopeMap[0].length();
     for (auto slope : slopes) {
         int col = 0;
         int nbrOfTrees = 0;
-        for (int row = 0; row < slopeMap.size(); row += slope[1]) {
-
-            if (slopeMap[row][col] == '#') {
-                //slopeMap[row][col] = 'X';
-                nbrOfTrees++;
-            } else {
-                //slopeMap[row][col] = 'O';
-            }
-
-            col += slope[0];
-            if (col > slopeMap[row].length() - 1) {
-                col = col - slopeMap[row].length();
-            }
+        for (int row = 0; row < slopeMap.size(); row += slope.second) {
+            nbrOfTrees += (slopeMap[row][col] == '#') ? 1 : 0;
+            col += slope.first;
+            col = (col >= slopeWidth) ? col - slopeWidth : col;
         }
         nbrOfTreesInSlopes.push_back(nbrOfTrees);
     }
 
-    unsigned long nbrOfTreesMulti = 1;
-    for (auto trees : nbrOfTreesInSlopes) {
-        nbrOfTreesMulti *= trees;
-    }
-    return nbrOfTreesMulti;
+    unsigned long nbrOfTreesMulti = std::accumulate(begin(nbrOfTreesInSlopes), end(nbrOfTreesInSlopes), 1, std::multiplies<>());
+    return (part_two ? nbrOfTreesMulti : nbrOfTreesInSlopes[1]);
 }
 
 #ifndef AoC_RUN_TEST
