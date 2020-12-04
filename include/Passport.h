c9f322a94417d732f9cf026925ca19b8db236a6e
Adam Ottvar <adam.ottvar@gmail.com>
2020-12-04 13:18:17 +0100

    Day4 part 2 solved but not nice

--- include/Passport.h
+++ include/Passport.h
@@ -13,18 +13,25 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <set>
 
 class Passport {
 private:
-    std::string byr;
-    std::string iyr;
-    std::string eyr;
+    int byr;
+    int iyr;
+    int eyr;
     std::string hgt;
     std::string hcl;
     std::string ecl;
     std::string pid;
-    std::string cid;
+    int cid;
     std::string rawData;
+
+    inline bool validateHeight();
+    inline bool validateHairColor();
+    inline bool validateEyeColor();
+    inline bool validatePassportID();
+    inline bool validateCountryID();
 
 public:
     inline Passport();
@@ -32,6 +39,7 @@ public:
     inline Passport(std::map<std::string, std::string>);
     //inline Passport(int,int,int,std::string,std::string,std::string,unsigned long,int);
 
+    inline bool validatePassportBasic(bool cidValid);
     inline bool validatePassport(bool cidValid);
     inline void print();
 
@@ -39,26 +47,26 @@ public:
 };
 
 Passport::Passport() {
-    byr = "";
-    iyr = "";
-    eyr = "";
+    byr = 0;
+    iyr = 0;
+    eyr = 0;
     hgt = "";
     hcl = "";
     ecl = "";
     pid = "";
-    cid = "";
+    cid = 0;
     rawData = "";
 }
 
 Passport::Passport(std::string raw) {
-    byr = "";
-    iyr = "";
-    eyr = "";
+    byr = 0;
+    iyr = 0;
+    eyr = 0;
     hgt = "";
     hcl = "";
     ecl = "";
     pid = "";
-    cid = "";
+    cid = 0;
     rawData = raw;
 }
 
@@ -66,14 +74,14 @@ Passport::Passport(std::map<std::string, std::string> inputData) {
     // for (std::pair<std::string,std::string> dat : inputData) {
     //     std::cout << dat.first << ": " << dat.second << std::endl;
     // }
-    byr = inputData["byr"];
-    iyr = inputData["iyr"];
-    eyr = inputData["eyr"];
+    byr = atoi(inputData["byr"].c_str());
+    iyr = atoi(inputData["iyr"].c_str());
+    eyr = atoi(inputData["eyr"].c_str());
     hgt = inputData["hgt"];
     hcl = inputData["hcl"];
     ecl = inputData["ecl"];
     pid = inputData["pid"];
-    cid = inputData["cid"];
+    cid = atoi(inputData["cid"].c_str());
 }
 
 /*
@@ -86,29 +94,96 @@ Passport::Passport(int byr, int iyr, int eyr, std::string hgt, std::string hcl, 
 }
 */
 
-bool Passport::validatePassport(bool cidValid) {
+bool Passport::validatePassportBasic(bool cidValid) {
     bool passwordValidates = false;
     int missingFileds = 0;
-    if (cidValid) {
-        missingFileds += byr == "";
-        missingFileds += iyr == "";
-        missingFileds += eyr == "";
-        missingFileds += hgt == "";
-        missingFileds += hcl == "";
-        missingFileds += ecl == "";
-        missingFileds += pid == "";
-    } else {
-        missingFileds += byr == "";
-        missingFileds += iyr == "";
-        missingFileds += eyr == "";
-        missingFileds += hgt == "";
-        missingFileds += hcl == "";
-        missingFileds += ecl == "";
-        missingFileds += pid == "";
-        missingFileds += cid == "";
+
+    missingFileds += byr == 0;
+    missingFileds += iyr == 0;
+    missingFileds += eyr == 0;
+    missingFileds += hgt == "";
+    missingFileds += hcl == "";
+    missingFileds += ecl == "";
+    missingFileds += pid == "";
+    if (!cidValid) {
+        missingFileds += cid == 0;
     }
 
     passwordValidates = (missingFileds == 0) ? true : false;
+
+    return passwordValidates;
+}
+
+inline bool Passport::validateHeight(){
+    int height;
+    if (hgt == "" || hgt.length() < 4) {
+        return false;
+    } else if (hgt.substr(hgt.length() - 2) == "in") {
+        std::string heightNumber = hgt.substr(0, hgt.length() - 2);
+        height = atoi(heightNumber.c_str());
+        return height >= 59 && height <= 76;
+    } else if (hgt.substr(hgt.length() - 2) == "cm") {
+        std::string heightNumber = hgt.substr(0, hgt.length() - 2);
+        height = atoi(heightNumber.c_str());
+        return height >= 150 && height <= 193;
+    } else {
+        return false;
+    }
+}
+
+inline bool Passport::validateHairColor(){
+    if (hcl == "" || hcl.length() != 7 || hcl[0] != '#') {
+        return false;
+    } else {
+        return std::all_of(hcl.begin() + 1, hcl.end(), [](char ch) { return (isdigit(ch) || (ch >= 'a' && ch <= 'f')); });
+    }
+}
+inline bool Passport::validateEyeColor(){
+    std::set<std::string> validEyeColors{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"};
+
+    if (validEyeColors.find(ecl) != validEyeColors.end())
+        return true;
+
+    return false;
+}
+inline bool Passport::validatePassportID(){
+    if (pid == "" || pid.length() != 9) {
+        return false;
+    } else {
+        return std::all_of(pid.begin(), pid.end(), [](char ch) { return isdigit(ch); });
+    }
+}
+inline bool Passport::validateCountryID(){
+    return true;
+}
+
+bool Passport::validatePassport(bool cidValid) {
+    bool passwordValidates = false;
+    int nonvalidFileds = 0;
+
+    nonvalidFileds += byr < 1920 || byr > 2002;
+    // cout << "byr: " << (byr < 1920 || byr > 2002) << ", " << std::flush;
+    nonvalidFileds += iyr < 2010 || iyr > 2020;
+    // cout << "iyr: " << (iyr < 2010 || iyr > 2020) << ", " << std::flush;
+    nonvalidFileds += eyr < 2020 || eyr > 2030;
+    // cout << "eyr: " << (eyr < 2020 || eyr > 2030) << ", " << std::flush;
+
+    nonvalidFileds += !Passport::validateHeight();
+    // cout << "hgt: " << !Passport::validateHeight() << ", " << std::flush;
+    nonvalidFileds += !Passport::validateHairColor();
+    // cout << "hcl: " << !Passport::validateHairColor() << ", " << std::flush;
+    nonvalidFileds += !Passport::validateEyeColor();
+    // cout << "ecl: " << !Passport::validateEyeColor() << ", " << std::flush;
+    nonvalidFileds += !Passport::validatePassportID();
+    // cout << "pid: " << !Passport::validatePassportID() << ", " << std::flush;
+
+    if (cidValid) {
+        nonvalidFileds += !Passport::validateCountryID();
+    }
+
+    // std::cout << "Fields:" << nonvalidFileds << std::endl;
+
+    passwordValidates = (nonvalidFileds == 0) ? true : false;
 
     return passwordValidates;
 }
@@ -124,16 +199,5 @@ void Passport::print() {
     std::cout << "cid:" << cid << " " << std::flush;
     std::cout << "rawData:" << rawData << " " << std::endl;
 }
-
-/*
-std::istream &operator>>(std::istream &input, Passport &P) {
-    std::string tempChar;
-    input >> P.start >> P.end >> tempChar >> P.password;
-    P.end = abs(P.end);
-    P.character = tempChar[0];
-
-    return input;
-}
-*/
 
 #endif //_PASSWORD_H_
--- src/day04.cpp
+++ src/day04.cpp
@@ -8,7 +8,9 @@
 #include <map>
 
 int day04(bool part_two) {
+#ifndef AoC_RUN_TEST
     cout << "AoC day04: part " << (part_two ? "two" : "one") << endl;
+#endif
     ifstream inFile("input/input04.txt");
     int result = 0;
     vector<Passport> passports;
@@ -20,8 +22,7 @@ int day04(bool part_two) {
                 Passport newPass = Passport(inputData);
                 passports.push_back(newPass);
                 inputData.clear();
-            } 
-            else {
+            } else {
                 std::stringstream ss(line);
                 while (getline(ss, line, ' ')) {
                     std::stringstream ss2(line);
@@ -39,19 +40,23 @@ int day04(bool part_two) {
             }
         }
 
-    } 
-    else {
+    } else {
         cout << "File not open" << endl;
     }
 
-    int validPassports = count_if(passports.begin(), passports.end(), [](Passport p) { return p.validatePassport(true); });
+    int validPassports;
+    if (!part_two)
+        validPassports = count_if(passports.begin(), passports.end(), [](Passport p) { return p.validatePassportBasic(true); });
+    else
+        validPassports = count_if(passports.begin(), passports.end(), [](Passport p) { return p.validatePassport(true); });
+
     return validPassports;
 }
 
 #ifndef AoC_RUN_TEST
 int main() {
     int result;
-    result = day04(false);
+    result = day04(true);
     cout << "Result: " << result << endl;
     return 0;
 }
